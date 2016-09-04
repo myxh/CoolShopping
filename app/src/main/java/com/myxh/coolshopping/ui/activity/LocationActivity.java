4ca01d37e5d1aef63bcb2da5d9ee9a33e386cf56
myxh <myxhchen@gmail.com>
2016-09-04 22:32:31 +0800

    add LocationActivity

--- app/src/main/java/com/myxh/coolshopping/ui/activity/LocationActivity.java
+++ app/src/main/java/com/myxh/coolshopping/ui/activity/LocationActivity.java
@@ -2,14 +2,105 @@ package com.myxh.coolshopping.ui.activity;
 
 import android.os.Bundle;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 import com.myxh.coolshopping.R;
 import com.myxh.coolshopping.ui.base.BaseActivity;
 
 public class LocationActivity extends BaseActivity {
 
+    LocationClient mLocationClient;
+    public MyLocationListener mLocationListener = new MyLocationListener();
+    private MapView mMapView;
+    private BaiduMap mBaiduMap;
+    private boolean isFirstLoc = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        initView();
+        initBaiduMap();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        //销毁定位
+        mLocationClient.stop();
+        //关闭定位图层
+        mBaiduMap.setMyLocationEnabled(false);
+        mMapView.onDestroy();
+        mMapView = null;
+        super.onDestroy();
+    }
+
+    private void initBaiduMap() {
+        //地图初始化
+        mBaiduMap = mMapView.getMap();
+        //开启定位图层
+        mBaiduMap.setMyLocationEnabled(true);
+        //定位初始化
+        mLocationClient = new LocationClient(this);
+        mLocationClient.registerLocationListener(mLocationListener);
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true);//打开GPS
+        option.setCoorType("bd0911");//设置坐标类型
+        option.setScanSpan(1000);
+        mLocationClient.setLocOption(option);
+        mLocationClient.start();
+    }
+
+    private void initView() {
+        mMapView = (MapView) findViewById(R.id.location_map_view);
+    }
+
+
+    /**
+     * 定位监听
+     */
+    public class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            //MapView被销毁之后不再接收新的位置
+            if (bdLocation == null || mMapView == null) {
+                return;
+            }
+            MyLocationData locationData = new MyLocationData.Builder()
+                    .accuracy(bdLocation.getRadius())//设置获取到的方向信息，顺时针0~360
+                    .direction(100)
+                    .latitude(bdLocation.getLatitude())
+                    .longitude(bdLocation.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locationData);
+            if (isFirstLoc) {
+                isFirstLoc = false;
+                LatLng latLng = new LatLng(bdLocation.getLatitude(),bdLocation.getLongitude());
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.target(latLng).zoom(18.0f);
+                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+            }
+        }
+
+        public void onReceivePoi(BDLocation poiLocation) {
+        }
     }
 }
